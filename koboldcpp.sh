bc46195382f9a5fcd83811da32c42a93885692de
Henk <henk@henk.tech>
2023-12-09 21:14:31 +0100

    koboldcpp.sh dist generator

--- koboldcpp.sh
+++ koboldcpp.sh
@@ -13,6 +13,8 @@ bin/micromamba run -r conda -n linux make LLAMA_OPENBLAS=1 LLAMA_CLBLAST=1 LLAMA
 
 if [[ $1 == "rebuild" ]]; then
 	echo Rebuild complete, you can now try to launch Koboldcpp.
+elif [[ $1 == "dist" ]]; then
+	bin/micromamba run -r conda -n linux pyinstaller --noconfirm --onefile --collect-all customtkinter --add-data='./koboldcpp_default.so:.' --add-data='./koboldcpp_cublas.so:.' --add-data='./koboldcpp_openblas.so:.' --add-data='./koboldcpp_clblast.so:.' --add-data='./koboldcpp_clblast_noavx2.so:.' --clean --console koboldcpp.py -n "koboldcpp.bin"
 else
 	bin/micromamba run -r conda -n linux python koboldcpp.py $*
 fi
