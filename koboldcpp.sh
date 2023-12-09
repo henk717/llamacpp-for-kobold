76cfb2bdb28ccec0b419c39212ab39c914bcb7c0
Henk <henk@henk.tech>
2023-12-09 20:23:01 +0100

    koboldcpp.sh polish

--- koboldcpp.sh
+++ koboldcpp.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-wget -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
-if [[ ! -f "conda/envs/linux/bin/python" || $1 == "rebuild" ]]; then
- bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
- bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
- bin/micromamba run -r conda -n linux make clean
+if [ ! -f "bin/micromamba" ]; then
+	wget -qO- https://anaconda.org/conda-forge/micromamba/1.5.3/download/linux-64/micromamba-1.5.3-0.tar.bz2 | tar -xvj bin/micromamba
 fi
+
+if [[ ! -f "conda/envs/linux/bin/python" || $1 == "rebuild" ]]; then
+	bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
+	bin/micromamba create --no-shortcuts -r conda -n linux -f environment.yaml -y
+	bin/micromamba run -r conda -n linux make clean
+fi
+
 bin/micromamba run -r conda -n linux make LLAMA_OPENBLAS=1 LLAMA_CLBLAST=1 LLAMA_CUBLAS=1 LLAMA_PORTABLE=1
-bin/micromamba run -r conda -n linux python koboldcpp.py $*
+
+if [[ $1 == "rebuild" ]]; then
+	echo Rebuild complete, you can now try to launch Koboldcpp.
+else
+	bin/micromamba run -r conda -n linux python koboldcpp.py $*
+fi
